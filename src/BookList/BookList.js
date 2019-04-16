e50a23c3c7c3ca4a4bd848bc0302d779c210bfc0
Ryan Carville <ryancarville@gmail.com>
2019-04-16 12:31:26 +0200

    change map function to object key

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -4,8 +4,8 @@ import './BookList.css';
 class BookList extends Component {
 	render() {
 		console.log(this.props);
-		const books = this.props.list.items.map((book, i) => {
-			return <li key={i}>{book.id}</li>;
+		const books = Object.keys(this.props.list).map((book, i) => {
+			return <li key={i}>{book[i].id}</li>;
 		});
 		return <div className='bookList'>{books}</div>;
 	}
