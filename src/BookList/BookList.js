18e6a240e8a7299c10b7a0c725374d9ab8f5f703
Ryan Carville <ryancarville@gmail.com>
2019-04-16 12:44:55 +0200

    update li

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -3,9 +3,10 @@ import './BookList.css';
 
 class BookList extends Component {
 	render() {
-		console.log(this.props);
-		const books = Object.keys(this.props.list).map((book, i) => {
-			return <li key={i}>{book.items[i].id}</li>;
+		const bookData = this.props.list;
+		console.log(bookData);
+		const books = Object.keys(bookData).map((book, i) => {
+			return <li key={book.items[i].id}>{book.items[i].volumeInfo.title}</li>;
 		});
 		return <div className='bookList'>{books}</div>;
 	}
